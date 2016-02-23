82d6ce43ea7ea6d57ee4a057f5230191b300e6ce
Mike Nichols <nichols.mike.s@gmail.com>
2016-02-23 15:10:21 -0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # min-webdriver-tap-client
-min-webdriver TAP client
+
+The client found in [min-webdriver](https://github.com/mantoni/min-webdriver) is only included when using 
+it as a browserify plugin. Furthermore, tests will hang if you are using TAP output.
+This client can be thrown at the top of your tests bundle which is piped into `min-wb`.
+
+## Install
+
+`npm install min-webdriver-tap-client`
+
+## Usage
+
+
+```js
+//spec-support.js
+
+const mwtc = require('min-webdriver-tap-client')
+const tape = require('blue-tape')
+mwtc(tape) // returns {brout,tape }
+```
+
+
+
---
+++ index.js
@@ -0,0 +1,78 @@
+/*
+ * min-webdriver-tap-client
+ * Original Copyright (c) 2014 Maximilian Antoni <mail@maxantoni.de>
+ * Copyright (c) 2016 Mike Nichols <nichols.mike.s@gmail.com>
+ *
+ * @license MIT
+ **/
+
+var brout   = require('brout');
+module.exports = function create(tape,opts) {
+
+/*global window*/
+'use strict';
+if(!tape) {
+    throw new Error('provide your tape impl')
+}
+if(!tape.onFinish) {
+    throw new Error('`onFinish` callback is required on tape impl')
+}
+opts = (opts || {});
+
+var logs    = [];
+var pending = null;
+var failure = false;
+var failRE = /^#\sfail\s.(\d)/
+
+
+function flush() {
+  pending(logs.join(''));
+  logs.length = 0;
+  pending = null;
+}
+
+function err(err) {
+  logs.push(err);
+  failure = true;
+  if (pending) {
+    flush();
+  }
+}
+function push(str) {
+  logs.push(str);
+  //detect first `fail` so we can report exit code of 1
+  failure = (failure || failRE.test(str))
+  if (pending) {
+    flush();
+  }
+}
+
+window._webdriver_poll = function (callback) {
+  pending = callback;
+  if (logs.length) {
+    flush();
+  }
+};
+
+window._webdriver_manualPoll = function () {
+  var str = logs.join('');
+  logs = [];
+  return str;
+};
+
+brout.on('out', push);
+brout.on('err', err);
+brout.on('exit', function (code) {
+  push('\nWEBDRIVER_EXIT(' + code + ')\n');
+});
+
+tape.onFinish(function(results){
+    brout.emit('exit',failure ? 1 : 0)
+})
+
+return {
+    brout: brout
+    , tape: tape
+}
+
+};
